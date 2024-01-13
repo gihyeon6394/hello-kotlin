50fe8386f0b4178c796ddf1364e065dfbcfcb79f
gimgihyeon <gihyeon6394@gmail.com>
2024-01-13 19:22:43 +0900

    Variables

--- src/main/kotlin/Ex.kt
+++ src/main/kotlin/Ex.kt
@@ -1,3 +1,26 @@
-fun main(){
+fun main() {
     println("Hello Kotlin!")
+
+    // Variables
+    // val : read-only variable
+    val popcorn = 5
+    val hotdog = 7
+    val customers = 10
+
+    // customers = 20 // Error: Val cannot be reassigned
+
+    // var : mutable variable
+    var total = 0
+    total = (popcorn * 3) + (hotdog * 2)
+    println("Total: $total")
+
+    // String templates
+    println("Total: $total")
+    println("there are ${customers * 100} customers")
+
+    // Practice
+
+    val name = "Mary"
+    val age = 20
+    println("$name is $age years old")
 }
