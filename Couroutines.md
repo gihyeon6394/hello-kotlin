c91bb7d151a75ae3847534e4b892970d72c6fc4e
gihyeon6394 <gihyeon6394@gmail.com>
2024-06-03 12:36:22 +0900

    Coroutine context and dispatchers > Parental responsibilities

--- Couroutines.md
+++ Couroutines.md
@@ -1705,6 +1705,34 @@ main: Who has survived request cancellation?
 job1: I am not affected by cancellation of the request
 ````
 
+### Parental responsibilities
+
+- 부모 코루틴은 자식 코루틴이 완료되기를 언제나 기다림
+- 명시적으로 `Job.join` 해서 기다리지 않아도 됨
+
+````kotlin
+// launch a coroutine to process some kind of incoming request
+val request = launch {
+    repeat(3) { i -> // launch a few children jobs
+        launch {
+            delay((i + 1) * 200L) // variable delay 200ms, 400ms, 600ms
+            println("Coroutine $i is done")
+        }
+    }
+    println("request: I'm done and I don't explicitly join my children that are still active")
+}
+request.join() // wait for completion of the request, including all its children
+println("Now processing of the request is complete")
+````
+
+````
+request: I'm done and I don't explicitly join my children that are still active
+Coroutine 0 is done
+Coroutine 1 is done
+Coroutine 2 is done
+Now processing of the request is complete
+````
+
 ## Asynchronous Flow
 
 ## Channels
--- src/main/kotlin/coroutines/Context2.kt
+++ src/main/kotlin/coroutines/Context2.kt
@@ -1,10 +1,7 @@
 package coroutines
 
+import kotlinx.coroutines.*
 import kotlinx.coroutines.GlobalScope.coroutineContext
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.newSingleThreadContext
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 
 fun main() {
@@ -20,4 +17,20 @@ fun main() {
             }
         }
     }
+
+    runBlocking {
+        // launch a coroutine to process some kind of incoming request
+        val request = launch {
+            repeat(3) { i -> // launch a few children jobs
+                launch {
+                    delay((i + 1) * 200L) // variable delay 200ms, 400ms, 600ms
+                    println("Coroutine $i is done")
+                }
+            }
+            println("request: I'm done and I don't explicitly join my children that are still active")
+        }
+        request.join() // wait for completion of the request, including all its children
+        println("Now processing of the request is complete")
+    }
+
 }
