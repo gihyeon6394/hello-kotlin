5cde92a8b292c0aa9deaf64618c0cd3c8805a2f3
gihyeon6394 <gihyeon6394@gmail.com>
2024-07-31 13:42:03 +0900

    코루틴 스코프 빌더의 차이 초안

--- src/main/kotlin/blog/cor/basic/exRunBlocking.kt
+++ src/main/kotlin/blog/cor/basic/exRunBlocking.kt
@@ -2,7 +2,6 @@ package blog.cor.basic
 
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 /**
@@ -25,14 +24,24 @@ fun main() = runBlocking {
 //    }
 //    println("Hello,")
 
-    coroutineScope {
-        launch {
-            delay(1000L)
-            println("World!")
-        }
-        println("Hello,")
-    }
+//    coroutineScope {
+//        launch {
+//            delay(1000L)
+//            println("World!")
+//        }
+//        println("Hello,")
+//    }
+    makeCoroutine()
+    notMakeCoroutine()
 }
 
+fun makeCoroutine() = runBlocking {
+    println("made coroutine")
+    delay(1000L)
+}
 
+suspend fun notMakeCoroutine() = coroutineScope {
+    println("not made coroutine")
+    delay(1000L)
+}
 
