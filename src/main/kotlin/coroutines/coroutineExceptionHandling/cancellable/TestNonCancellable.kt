1bfeb1e5425d9c461c629357cf571b9351981acb
gihyeon6394 <gihyeon6394@gmail.com>
2024-11-07 13:32:04 +0900

    코루틴 취소하기 디깅 2 : suspend와 cancel

--- src/main/kotlin/coroutines/coroutineExceptionHandling/cancellable/TestNonCancellable.kt
+++ src/main/kotlin/coroutines/coroutineExceptionHandling/cancellable/TestNonCancellable.kt
@@ -2,9 +2,10 @@ import kotlinx.coroutines.*
 
 fun main() = runBlocking {
 
-    testBasic()
+//    testBasic()
 //    test2()
 //    test3()
+    testCancelButNotCancelled()
 }
 
 fun testBasic() = runBlocking {
@@ -23,9 +24,9 @@ fun testBasic() = runBlocking {
 
 
 // 코루틴을 실행하던 중 해당 코루틴이 취소되어도,
-// NonCancellable을 사용하면 해당 코루틴은 반드시 해당 블럭을 실행하게 된다.
+// finally 블럭은 반드시 실행된다.
 fun test2() = runBlocking {
-    val job = launch (Dispatchers.Default){
+    val job = launch {
         try {
             repeat(1000) { i ->
                 println("I'm sleeping $i ...")
@@ -33,6 +34,11 @@ fun test2() = runBlocking {
             }
         } finally {
             println("I'm running finally")
+            withContext(NonCancellable) {
+                println("this is non-cancellable block start")
+                delay(1000L)
+                println("this is non-cancellable block end")
+            }
         }
     }
 
@@ -64,5 +70,22 @@ fun test3() = runBlocking {
     delay(1300L) // delay a bit
     println("main: I'm tired of waiting!")
     job2.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+}
+
+
+/**
+ * 코루틴을 취소시켜도 취소되지 않고 계속되는 경우
+ * 쿠루틴의 취소여부는 내부에서 suspend 함수를 호출할때마다 체크하기 때문
+ * -> suspend 함수가 호출되지 않으면 취소되지 않는다.
+ */
+fun testCancelButNotCancelled() = runBlocking {
+    val job = launch {
+        repeat(1000) { i ->
+            println("I'm sleeping $i ...")
+        }
+    }
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // 취소 후 기다림
     println("main: Now I can quit.")
 }
