c8f2017d308d57f39b99b428f4c5673e9d60e43b
gihyeon6394 <gihyeon6394@gmail.com>
2024-12-18 13:12:05 +0900

    코루틴 스코프에서 스레드 블로킹해보기 가다듬기

--- src/main/kotlin/coroutines/myself/til/cor20241212.kt
+++ src/main/kotlin/coroutines/myself/til/cor20241212.kt
@@ -8,16 +8,18 @@ import kotlinx.coroutines.*
 val singleThreadContext = newSingleThreadContext("singleThreadContext")
 
 /**
+ * suspend 함수 안에서 스레드를 블로킹하면 발생하는 일
+ *
+ * 결론 : suspend (코루틴 중지 가능 함수) 함수여도 스레드를 블로킹하면 throughput (처리량) 떨어진다.
  *
  * job2에서 스레드를 블로킹하는 바람에 job1을 실행할 스레드가 없다.
- * 즉 suspend 함수에서 스레드를 블로킹하면 안된다.
  */
 fun main() = runBlocking {
     withContext(singleThreadContext) {
         val job1 = async {
-            someLogic()
+            some_logic()
         }
-        delay(1000)
+        delay(200)
         val job2 = async {
             suspended_but_block_thread()
         }
@@ -26,16 +28,16 @@ fun main() = runBlocking {
 
 suspend fun suspended_but_block_thread() = coroutineScope {
     println("block thread ..")
-//    Thread.sleep(1000)
-    delay(1000)
+    Thread.sleep(1000) // Replace this "Thread.sleep()" call with "delay()".
+//    delay(1000)
     println("release thread ..")
 }
 
-suspend fun someLogic() = coroutineScope {
-    println("begin someLogic ..")
+suspend fun some_logic() = coroutineScope {
+    println("begin some logic ..")
     repeat(10) {
-        delay(500)
+        delay(50)
         println("[$it] some logic .. ")
     }
-    println("end someLogic ..")
+    println("end some logic ..")
 }
