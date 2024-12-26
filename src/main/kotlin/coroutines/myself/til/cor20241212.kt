fd5986490783f250d93403b45538394e0e93b4f1
gihyeon6394 <gihyeon6394@gmail.com>
2024-12-26 12:59:30 +0900

    코루틴 스코프에서 스레드 블로킹해보기 가다듬기 2

--- src/main/kotlin/coroutines/myself/til/cor20241212.kt
+++ src/main/kotlin/coroutines/myself/til/cor20241212.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.*
 /**
  * @author gihyeon-kim
  */
-val singleThreadContext = newSingleThreadContext("singleThreadContext")
 
 /**
  * suspend 함수 안에서 스레드를 블로킹하면 발생하는 일
@@ -14,30 +13,34 @@ val singleThreadContext = newSingleThreadContext("singleThreadContext")
  *
  * job2에서 스레드를 블로킹하는 바람에 job1을 실행할 스레드가 없다.
  */
+
+val singleThreadContext = newSingleThreadContext("singleThreadContext")
+
 fun main() = runBlocking {
     withContext(singleThreadContext) {
         val job1 = async {
-            some_logic()
+            logic_1()
         }
         delay(200)
         val job2 = async {
-            suspended_but_block_thread()
+            logic_2_blocking_thread()
         }
     }
 }
 
-suspend fun suspended_but_block_thread() = coroutineScope {
-    println("block thread ..")
-    Thread.sleep(1000) // Replace this "Thread.sleep()" call with "delay()".
-//    delay(1000)
-    println("release thread ..")
-}
-
-suspend fun some_logic() = coroutineScope {
+suspend fun logic_1() = coroutineScope {
     println("begin some logic ..")
     repeat(10) {
         delay(50)
         println("[$it] some logic .. ")
     }
     println("end some logic ..")
+}
+
+
+suspend fun logic_2_blocking_thread() = coroutineScope {
+    println("block thread ..")
+    Thread.sleep(1000) // Replace this "Thread.sleep()" call with "delay()".
+//    delay(1000)
+    println("release thread ..")
 }
